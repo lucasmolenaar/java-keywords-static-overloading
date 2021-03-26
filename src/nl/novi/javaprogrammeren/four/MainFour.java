6502ee9b3294786c87c4e1dcdb584d4fe8ca4ec8
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-03-26 14:45:10 +0100

    finished four

--- src/nl/novi/javaprogrammeren/four/MainFour.java
+++ src/nl/novi/javaprogrammeren/four/MainFour.java
@@ -1,7 +1,15 @@
 package nl.novi.javaprogrammeren.four;
 
+import nl.novi.javaprogrammeren.four.hidden.Human;
+import nl.novi.javaprogrammeren.four.hidden.HumanMaker;
+
 public class MainFour {
+
     public static void main(String[] args) {
+        HumanMaker.createHuman("Lucas1");
+        Human lucas = HumanMaker.createHuman("Lucas2");
+
+        System.out.println(HumanMaker.getName(lucas));
 
 
     }
--- src/nl/novi/javaprogrammeren/four/hidden/HumanMaker.java
+++ src/nl/novi/javaprogrammeren/four/hidden/HumanMaker.java
@@ -2,6 +2,16 @@ package nl.novi.javaprogrammeren.four.hidden;
 
 public class HumanMaker {
 
-    private HumanMaker() {
+    private Human human;
+
+    public HumanMaker() {
+    }
+
+    public static Human createHuman(String name) {
+        return new Human("Lucas");
+    }
+
+    public static String getName(Human human) {
+        return human.getName();
     }
 }
